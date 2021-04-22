d9bd268a8ca31918c1fe3de60d719203e742d19b
Fabrice Brito <fabrice.brito@terradue>
2021-04-22 22:27:20 +0000

    readme + tests

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -1,8 +1,16 @@
+
+curl $1 -o temp.cwl
+
 cwl-wrapper \
     --stagein stagein-stars.cwl_ \
     --stageout stageout-stars.cwl_ \
-    $1 > m-$1
+    temp.cwl > m-temp.cwl
 
-cwltool --outdir test-execution m-$1#stage-manager $2
+cwltool --outdir \
+        test-execution \
+        m-temp.cwl#stage-manager \
+        $2
 
-rm -f m-$1
+rm -f \
+    m-temp.cwl \
+    temp.cwl
